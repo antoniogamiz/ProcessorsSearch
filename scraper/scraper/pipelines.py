016a113b68f96016881809eb8702b87a5b6132fe
Antonio <antoniogamiz10@gmail.com>
2017-12-16 11:17:36 +0100

    structure change

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class ScraperPipeline(object):
+class ProcessorsPipeline(object):
     def process_item(self, item, spider):
         return item
--- scraper/scraper/spiders/processors_spider.py
+++ scraper/scraper/spiders/processors_spider.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scraper.items import ProcessorsItem
+from scrapy.loader import ItemLoader
 
 class ProcessorsSpider(scrapy.Spider):
     name = "processors"
     start_urls = [
-        "https://www.pccomponentes.com/procesadores/",
+        "https://www.pccomponentes.com/procesadores",
     ]
 
     def parse(self, response):
-        for href in response.xpath("//a"):    # Analizamos cada post de una página.
-            yield {
-                "funciona" : "dddd",
-            }
+        for href in response.xpath("/html/body/div[1]/div[2]/div/div/div[2]/div/div[4]/div/div/article/div[1]/a"):    # Analizamos cada post de una página.
+            yield response.follow(href, self.parse_processors)
+
+    def parse_processors(self, response):
+            loader = ItemLoader(ProcessorsItem(), selector=response)
+            loader.add_xpath('brand', '/html/body/div[1]/div[2]/div[1]/div[2]/div/div/div[1]/h1/strong/text()')
+            return loader.load_item()
